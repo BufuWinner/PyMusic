18af1660fd7b8913a09307de960c1582b69a35d9
BufuWinner <airdroid.prova@gmail.com>
2020-12-27 22:16:23 +0100

    Added settings menu

--- deezer_en.py
+++ deezer_en.py
@@ -37,7 +37,7 @@ def global_search(query):
         # print(response)
 
         if len(response['tracks']['items']) == 0:
-            new = input('No more results, try again (empty = yes, e = exit)?\n')
+            new = input('There are no more results, retry? (empty = yes, e = exit)\n')
         if new == 'e':
             print('\nOk, goodbye!')
             exit()
@@ -62,7 +62,7 @@ def global_search(query):
 
         # print(ind_search)
         # print(res_search)
-        final_res = input('\nWhich track (insert number, leave empty to try again, n = next, e = exit)?\n')
+        final_res = input('\nWhich song? (insert number, leave empty to retry, n = next, e = exit)\n')
         if len(final_res) == 0:
             again = True
             offset = 0
@@ -72,7 +72,7 @@ def global_search(query):
         elif final_res == 'e':
             print('\nOk, goodbye!')
             quit()
-        elif int(final_res) not in range(index):
+        elif final_res not in range(index) or not final_res.isdigit():
             print('Number outrange :(')
             continue
         else:
@@ -83,128 +83,217 @@ def global_search(query):
     return link
 
 
-# DOWNLOAD INFO
-standard_output = '/Desktop'
-file_mover = True
+def settings():
+    while True:
+        with open('settings.txt', 'r') as settings:
+            lines = []
+            for line in settings:
+                print(line, end='')
+                lines.append(line)
+            setting = input('\nWhich option do you want to change? (e = exit)\n')
+            if setting == 'e':
+                break
+            elif setting == '1':
+                if 'True' in lines[0]:
+                    new = lines[0].replace('True', 'False')
+                    lines.pop(0)
+                    lines.insert(0, new)
+                else:
+                    new = lines[0].replace('False', 'True')
+                    lines.pop(0)
+                    lines.insert(0, new)
+            elif setting == '2':
+                if 'True' in lines[1]:
+                    new = lines[1].replace('True', 'False')
+                    lines.pop(1)
+                    lines.insert(1, new)
+                else:
+                    new = lines[1].replace('False', 'True')
+                    lines.pop(1)
+                    lines.insert(1, new)
+            elif setting == '3':
+                path = input('Inserisci il nuovo percorso=\n')
+                lines.pop(2)
+                lines.insert(2, f'3) output = {path}\n')
+            elif setting == '4':
+                print(f'\n{generate_token()}\n')
+            else:
+                print('I wasn\'t expecting this... only from 1 to 4')
+                return
+            new_settings = ''.join(lines)
+            # print(new_settings)
+        rp = open('settings.txt', 'w')
+        rp.write(new_settings)
+        rp.close()
 
-search_type = input('Global search (leave empty for specific search, links are accepted(album, playlist, track):\n')
-tracklink = None
-albumlink = None
-playlink = None
-link = ''
-artist = ''
-song = ''
 
-if 'https://open.spotify.com/' in search_type:
-    link = search_type
-    if '/track/' in search_type:
+def get_settings():
+    with open('settings.txt', 'r') as raw_settings:
+        settings = []
+        ind = 0
+        for line in raw_settings:
+            if ind == 3:
+                break
+            settings.append(line.split('= ')[-1].strip())
+            ind += 1
+
+        # settings.pop(-1)
+    return settings
+
+
+def check_output(output):
+    if not os.path.isdir(output):
+        a = input('The given output folder doesn\'t exist, do you want to create it? (y/n)\n')
+        if a == 'y':
+            checked = output
+        elif a == 'n':
+            new_out = input('new output= ')
+            if not os.path.isdir(new_out):
+                print('Nevermind, just put a valid output in the options or while searching.')
+                quit()
+            checked = new_out
+        else:
+            print('I\'ll get that as a yes...')
+            checked = output
+        return checked
+    return output
+
+
+while True:
+    # DOWNLOAD INFO
+    repeat, file_mover, standard_output = get_settings()
+    # print(repeat, file_mover, standard_output)
+    repeat = str(repeat)
+    file_mover = str(file_mover)
+
+    search_type = input('Global search (leave empty for specific search, --options to enter the settings):\n')
+    tracklink = None
+    albumlink = None
+    playlink = None
+    deezlink = None
+    link = ''
+    artist = ''
+    song = ''
+
+    if search_type == '--options':
+        settings()
+        continue
+
+    elif 'https://open.spotify.com/' in search_type:
+        link = search_type
+        if '/track/' in search_type:
+            tracklink = True
+            albumlink = False
+        elif '/album/' in search_type:
+            albumlink = True
+        elif '/playlist/' in search_type:
+            playlink = True
+            albumlink = False
+
+    elif len(search_type) == 0:
+        song = input('Track= ')
+        artist = input('Artist= ')
+        tracklink = False
+        albumlink = False
+
+    else:
+        link = global_search(search_type)
+
         tracklink = True
         albumlink = False
-    elif '/album/' in search_type:
-        albumlink = True
-    elif '/playlist/' in search_type:
-        playlink = True
-        albumlink = False
+    out = input('Change output? (leave empty for standard)\n')
+    if len(out) == 0:
+        output = check_output(standard_output)
+    else:
+        output = check_output(out)
 
-elif len(search_type) == 0:
-    song = input('Track= ')
-    artist = input('Artist= ')
-    tracklink = False
-    albumlink = False
+    # DEEZER DOWNLOADER
+    download = deezloader.Login("il tuo ARL token")
+    if tracklink:
+        try:
+            download.download_trackspo(
+                URL=link,
+                output=output,
+                quality="MP3_320",
+                recursive_quality=True,
+                recursive_download=False,
+                not_interface=False
+            )
+        except(deezloader.exceptions.TrackNotFound, deezloader.exceptions.NoDataApi, deezloader.exceptions.InvalidLink):
+            print('Couldn\'t find the song :(\n')
+            quit()
 
-else:
-    link = global_search(search_type)
+    elif albumlink:
+        try:
+            download.download_albumspo(
+                URL=link,
+                output=output,
+                quality="MP3_320",
+                recursive_quality=True,
+                recursive_download=False,
+                not_interface=False,
+                zips=False
+            )
+            file_mover = False
+        except(deezloader.exceptions.TrackNotFound, deezloader.exceptions.NoDataApi, deezloader.exceptions.InvalidLink):
+            print('Couldn\'t find the song :(\n')
+            quit()
 
-    tracklink = True
-    albumlink = False
-out = input('Change output (leave empty for standard)?\n')
-if len(out) == 0:
-    output = standard_output
-else:
-    output = out
+    elif playlink:
+        try:
+            download.download_playlistspo(
+                URL=link,
+                output=output,
+                quality="MP3_320",
+                recursive_quality=True,
+                recursive_download=False,
+                not_interface=False,
+                zips=False
+            )
+            file_mover = False
+        except(deezloader.exceptions.TrackNotFound, deezloader.exceptions.NoDataApi, deezloader.exceptions.InvalidLink):
+            print('Couldn\'t find the song :(\n')
+            quit()
 
-# DEEZER DOWNLOADER
-download = deezloader.Login("your ARL token")
-if tracklink:
-    try:
-        download.download_trackspo(
-            URL=link,
-            output=output,
-            quality="MP3_320",
-            recursive_quality=True,
-            recursive_download=False,
-            not_interface=False
-        )
-    except(deezloader.exceptions.TrackNotFound, deezloader.exceptions.NoDataApi, deezloader.exceptions.InvalidLink):
-        print('Track not found :(\n')
-        quit()
+    else:
+        try:
+            download.download_name(
+                artist=artist.lower(),
+                song=song.lower(),
+                output=output,
+                quality="MP3_320",
+                recursive_quality=True,
+                recursive_download=False,
+                not_interface=False
+            )
+        except(deezloader.exceptions.TrackNotFound, deezloader.exceptions.NoDataApi):
+            print('Couldn\'t find the song :(\n')
+            quit()
 
-elif albumlink:
-    try:
-        download.download_albumspo(
-            URL=link,
-            output=output,
-            quality="MP3_320",
-            recursive_quality=True,
-            recursive_download=False,
-            not_interface=False,
-            zips=False
-        )
-        file_mover = False
-    except(deezloader.exceptions.TrackNotFound, deezloader.exceptions.NoDataApi, deezloader.exceptions.InvalidLink):
-        print('Track not found :(\n')
-        quit()
+    # File Mover
+    if file_mover == 'True':
+        project_dir = os.getcwd()
+        files = glob.glob(f'{output}/*')
+        folder = max(files, key=os.path.getmtime)
+        os.chdir(folder)
+        list2 = os.listdir()
+        song_move = list2[0]
+        number = 1
+        for _ in range(len(files)):
+            if os.path.isfile(f'{output}/{number}.mp3'):
+                number += 1
+            else:
+                break
+        try:
+            os.rename(song_move, f'{output}/{number}.mp3')
+        except:
+            print('Couldn\'t move song :(')
+            quit()
+        print('\nSong moved!')
 
-elif playlink:
-    try:
-        download.download_playlistspo(
-            URL=link,
-            output=output,
-            quality="MP3_320",
-            recursive_quality=True,
-            recursive_download=False,
-            not_interface=False,
-            zips=False
-        )
-        file_mover = False
-    except(deezloader.exceptions.TrackNotFound, deezloader.exceptions.NoDataApi, deezloader.exceptions.InvalidLink):
-        print('Track not found :(\n')
-        quit()
-
-else:
-    try:
-        download.download_name(
-            artist=artist.lower(),
-            song=song.lower(),
-            output=output,
-            quality="MP3_320",
-            recursive_quality=True,
-            recursive_download=False,
-            not_interface=False
-        )
-    except(deezloader.exceptions.TrackNotFound, deezloader.exceptions.NoDataApi):
-        print('Track not found :(\n')
-        quit()
-
-# File Mover
-if file_mover:
-    files = glob.glob(f'{output}/*')
-    folder = max(files, key=os.path.getmtime)
-    os.chdir(folder)
-    list2 = os.listdir()
-    song_move = list2[0]
-    number = 1
-    for _ in range(len(files)):
-        if os.path.isfile(f'{output}/{number}.mp3'):
-            number += 1
-        else:
-            break
-    try:
-        os.rename(song_move, f'{output}/{number}.mp3')
-    except:
-        print('Couldn\'t move track :(')
-        quit()
-    print('\nMoved track!')
-
-    os.rmdir(folder)
-    print('Deleted folder!\n')
+        os.rmdir(folder)
+        print('Folder deleted!\n')
+        if repeat == 'True':
+            os.chdir(project_dir)
+    if repeat == 'False':
+        break
--- deezer_it.py
+++ deezer_it.py
@@ -72,7 +72,7 @@ def global_search(query):
         elif final_res == 'e':
             print('\nOk, alla prossima!')
             quit()
-        elif int(final_res) not in range(index):
+        elif final_res not in range(index) or not final_res.isdigit():
             print('Numero invalido :(')
             continue
         else:
@@ -83,128 +83,217 @@ def global_search(query):
     return link
 
 
-# DOWNLOAD INFO
-standard_output = '/Desktop'
-file_mover = True
+def settings():
+    while True:
+        with open('settings.txt', 'r') as settings:
+            lines = []
+            for line in settings:
+                print(line, end='')
+                lines.append(line)
+            setting = input('\nQuale opzione vuoi cambiare?(e = exit)\n')
+            if setting == 'e':
+                break
+            elif setting == '1':
+                if 'True' in lines[0]:
+                    new = lines[0].replace('True', 'False')
+                    lines.pop(0)
+                    lines.insert(0, new)
+                else:
+                    new = lines[0].replace('False', 'True')
+                    lines.pop(0)
+                    lines.insert(0, new)
+            elif setting == '2':
+                if 'True' in lines[1]:
+                    new = lines[1].replace('True', 'False')
+                    lines.pop(1)
+                    lines.insert(1, new)
+                else:
+                    new = lines[1].replace('False', 'True')
+                    lines.pop(1)
+                    lines.insert(1, new)
+            elif setting == '3':
+                path = input('Inserisci il nuovo percorso=\n')
+                lines.pop(2)
+                lines.insert(2, f'3) output = {path}\n')
+            elif setting == '4':
+                print(f'\n{generate_token()}\n')
+            else:
+                print('Non mi aspettavo questo... solo dal 1 al 4')
+                return
+            new_settings = ''.join(lines)
+            # print(new_settings)
+        rp = open('settings.txt', 'w')
+        rp.write(new_settings)
+        rp.close()
 
-search_type = input('Ricerca globale (lasciare vuoto per ricerca specifica):\n')
-tracklink = None
-albumlink = None
-playlink = None
-link = ''
-artist = ''
-song = ''
 
-if 'https://open.spotify.com/' in search_type:
-    link = search_type
-    if '/track/' in search_type:
+def get_settings():
+    with open('settings.txt', 'r') as raw_settings:
+        settings = []
+        ind = 0
+        for line in raw_settings:
+            if ind == 3:
+                break
+            settings.append(line.split('= ')[-1].strip())
+            ind += 1
+
+        # settings.pop(-1)
+    return settings
+
+
+def check_output(output):
+    if not os.path.isdir(output):
+        a = input('La cartella di output data non esiste, crearla? (y/n)\n')
+        if a == 'y':
+            checked = output
+        elif a == 'n':
+            new_out = input('nuovo output= ')
+            if not os.path.isdir(new_out):
+                print('Non importa, metti un output valido nelle opzioni o in quello temporaneo.')
+                quit()
+            checked = new_out
+        else:
+            print('Lo prendo come un sì')
+            checked = output
+        return checked
+    return output
+
+
+while True:
+    # DOWNLOAD INFO
+    repeat, file_mover, standard_output = get_settings()
+    # print(repeat, file_mover, standard_output)
+    repeat = str(repeat)
+    file_mover = str(file_mover)
+
+    search_type = input('Ricerca globale (lasciare vuoto per ricerca specifica):\n')
+    tracklink = None
+    albumlink = None
+    playlink = None
+    deezlink = None
+    link = ''
+    artist = ''
+    song = ''
+
+    if search_type == '--options':
+        settings()
+        continue
+
+    elif 'https://open.spotify.com/' in search_type:
+        link = search_type
+        if '/track/' in search_type:
+            tracklink = True
+            albumlink = False
+        elif '/album/' in search_type:
+            albumlink = True
+        elif '/playlist/' in search_type:
+            playlink = True
+            albumlink = False
+
+    elif len(search_type) == 0:
+        song = input('Brano= ')
+        artist = input('Artista= ')
+        tracklink = False
+        albumlink = False
+
+    else:
+        link = global_search(search_type)
+
         tracklink = True
         albumlink = False
-    elif '/album/' in search_type:
-        albumlink = True
-    elif '/playlist/' in search_type:
-        playlink = True
-        albumlink = False
+    out = input('Cambiare Output (lasciare vuoto per standard)?\n')
+    if len(out) == 0:
+        output = check_output(standard_output)
+    else:
+        output = check_output(out)
 
-elif len(search_type) == 0:
-    song = input('Brano= ')
-    artist = input('Artista= ')
-    tracklink = False
-    albumlink = False
+    # DEEZER DOWNLOADER
+    download = deezloader.Login("il tuo ARL token")
+    if tracklink:
+        try:
+            download.download_trackspo(
+                URL=link,
+                output=output,
+                quality="MP3_320",
+                recursive_quality=True,
+                recursive_download=False,
+                not_interface=False
+            )
+        except(deezloader.exceptions.TrackNotFound, deezloader.exceptions.NoDataApi, deezloader.exceptions.InvalidLink):
+            print('Brano Non Trovato :(\n')
+            quit()
 
-else:
-    link = global_search(search_type)
+    elif albumlink:
+        try:
+            download.download_albumspo(
+                URL=link,
+                output=output,
+                quality="MP3_320",
+                recursive_quality=True,
+                recursive_download=False,
+                not_interface=False,
+                zips=False
+            )
+            file_mover = False
+        except(deezloader.exceptions.TrackNotFound, deezloader.exceptions.NoDataApi, deezloader.exceptions.InvalidLink):
+            print('Brano Non Trovato :(\n')
+            quit()
 
-    tracklink = True
-    albumlink = False
-out = input('Cambiare Output (lasciare vuoto per standard)?\n')
-if len(out) == 0:
-    output = standard_output
-else:
-    output = out
+    elif playlink:
+        try:
+            download.download_playlistspo(
+                URL=link,
+                output=output,
+                quality="MP3_320",
+                recursive_quality=True,
+                recursive_download=False,
+                not_interface=False,
+                zips=False
+            )
+            file_mover = False
+        except(deezloader.exceptions.TrackNotFound, deezloader.exceptions.NoDataApi, deezloader.exceptions.InvalidLink):
+            print('Brano Non Trovato :(\n')
+            quit()
 
-# DEEZER DOWNLOADER
-download = deezloader.Login("il tuo ARL token")
-if tracklink:
-    try:
-        download.download_trackspo(
-            URL=link,
-            output=output,
-            quality="MP3_320",
-            recursive_quality=True,
-            recursive_download=False,
-            not_interface=False
-        )
-    except(deezloader.exceptions.TrackNotFound, deezloader.exceptions.NoDataApi, deezloader.exceptions.InvalidLink):
-        print('Brano Non Trovato :(\n')
-        quit()
+    else:
+        try:
+            download.download_name(
+                artist=artist.lower(),
+                song=song.lower(),
+                output=output,
+                quality="MP3_320",
+                recursive_quality=True,
+                recursive_download=False,
+                not_interface=False
+            )
+        except(deezloader.exceptions.TrackNotFound, deezloader.exceptions.NoDataApi):
+            print('Brano Non Trovato :(\n')
+            quit()
 
-elif albumlink:
-    try:
-        download.download_albumspo(
-            URL=link,
-            output=output,
-            quality="MP3_320",
-            recursive_quality=True,
-            recursive_download=False,
-            not_interface=False,
-            zips=False
-        )
-        file_mover = False
-    except(deezloader.exceptions.TrackNotFound, deezloader.exceptions.NoDataApi, deezloader.exceptions.InvalidLink):
-        print('Brano Non Trovato :(\n')
-        quit()
+    # File Mover
+    if file_mover == 'True':
+        project_dir = os.getcwd()
+        files = glob.glob(f'{output}/*')
+        folder = max(files, key=os.path.getmtime)
+        os.chdir(folder)
+        list2 = os.listdir()
+        song_move = list2[0]
+        number = 1
+        for _ in range(len(files)):
+            if os.path.isfile(f'{output}/{number}.mp3'):
+                number += 1
+            else:
+                break
+        try:
+            os.rename(song_move, f'{output}/{number}.mp3')
+        except:
+            print('Brano non spostabile :(')
+            quit()
+        print('\nBrano spostato!')
 
-elif playlink:
-    try:
-        download.download_playlistspo(
-            URL=link,
-            output=output,
-            quality="MP3_320",
-            recursive_quality=True,
-            recursive_download=False,
-            not_interface=False,
-            zips=False
-        )
-        file_mover = False
-    except(deezloader.exceptions.TrackNotFound, deezloader.exceptions.NoDataApi, deezloader.exceptions.InvalidLink):
-        print('Brano Non Trovato :(\n')
-        quit()
-
-else:
-    try:
-        download.download_name(
-            artist=artist.lower(),
-            song=song.lower(),
-            output=output,
-            quality="MP3_320",
-            recursive_quality=True,
-            recursive_download=False,
-            not_interface=False
-        )
-    except(deezloader.exceptions.TrackNotFound, deezloader.exceptions.NoDataApi):
-        print('Brano Non Trovato :(\n')
-        quit()
-
-# File Mover
-if file_mover:
-    files = glob.glob(f'{output}/*')
-    folder = max(files, key=os.path.getmtime)
-    os.chdir(folder)
-    list2 = os.listdir()
-    song_move = list2[0]
-    number = 1
-    for _ in range(len(files)):
-        if os.path.isfile(f'{output}/{number}.mp3'):
-            number += 1
-        else:
-            break
-    try:
-        os.rename(song_move, f'{output}/{number}.mp3')
-    except:
-        print('Brano non spostabile :(')
-        quit()
-    print('\nBrano spostato!')
-
-    os.rmdir(folder)
-    print('Cartella eliminata!\n')
+        os.rmdir(folder)
+        print('Cartella eliminata!\n')
+        if repeat == 'True':
+            os.chdir(project_dir)
+    if repeat == 'False':
+        break
